b20c5a4f6d015743d7f79a489a5ce56ed74e52db
Alec Loudenback <alecloudenback@gmail.com>
2018-09-02 09:15:49 -0500

    clean up test file in preparation of other tables

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,15 +2,15 @@ import Test
 import MortalityTables
 
 tables = MortalityTables.Tables()
-cso = tables["2001 CSO Super Preferred Select and Ultimate - Male Nonsmoker, ANB"]
-vbt = tables["2001 VBT Residual Standard Select and Ultimate - Male Nonsmoker, ANB"]
+cso2001 = tables["2001 CSO Super Preferred Select and Ultimate - Male Nonsmoker, ANB"]
+vbt2001 = tables["2001 VBT Residual Standard Select and Ultimate - Male Nonsmoker, ANB"]
 
-Test.@test MortalityTables.qx(cso,35,1) == .00037
-Test.@test MortalityTables.qx(cso,35,61) == .26719
-Test.@test MortalityTables.qx(cso,95) == .26719
-Test.@test ismissing(MortalityTables.qx(cso,35,95))
+Test.@test MortalityTables.qx(cso2001,35,1) == .00037
+Test.@test MortalityTables.qx(cso2001,35,61) == .26719
+Test.@test MortalityTables.qx(cso2001,95) == .26719
+Test.@test ismissing(MortalityTables.qx(cso2001,35,95))
 
-Test.@test MortalityTables.qx(vbt,35,1) == .00036
-Test.@test MortalityTables.qx(vbt,35,61) == .24298
-Test.@test MortalityTables.qx(vbt,95) == .24298
-Test.@test ismissing(MortalityTables.qx(vbt,35,95))
+Test.@test MortalityTables.qx(vbt2001,35,1) == .00036
+Test.@test MortalityTables.qx(vbt2001,35,61) == .24298
+Test.@test MortalityTables.qx(vbt2001,95) == .24298
+Test.@test ismissing(MortalityTables.qx(vbt2001,35,95))
