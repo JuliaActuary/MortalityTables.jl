c6f914b7bcf85e86612d2f7c221347a50cd6bba0
Alec Loudenback <alecloudenback@gmail.com>
2020-09-26 06:14:40 -0500

    add new depenedencies for CSV

--- src/MortalityTables.jl
+++ src/MortalityTables.jl
@@ -1,7 +1,9 @@
 module MortalityTables
+using CSV
 using HTTP
 using Transducers
 using OffsetArrays
+using Parsers
 using QuadGK
 using UnPack
 using XMLDict
@@ -10,6 +12,7 @@ include("MetaData.jl")
 include("death_distribution.jl")
 include("MortalityTable.jl")
 include("XTbML.jl")
+include("CSV.jl")
 include("get_SOA_table.jl")
 include("parameterized_models.jl")
 
